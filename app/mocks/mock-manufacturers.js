0243bf475577281186f863da19d375a041c7f107
pburgos <pburgos@corpora360.com>
2016-04-12 13:17:56 +0200

    Ignore .js and .map.js files

--- app/mocks/mock-manufacturers.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by pablo on 12/04/16.
- */
-//# sourceMappingURL=mock-manufacturers.js.map
--- app/services/manufacturer.service.js
+++
@@ -1,4 +0,0 @@
-/**
- * Created by pablo on 12/04/16.
- */
-//# sourceMappingURL=manufacturer.service.js.map
