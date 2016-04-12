c8455d97f16f7c5cbb4284c5f2fd4dd89d8f149d
pburgos <pburgos@corpora360.com>
2016-04-12 13:17:29 +0200

    Add manufacturer service

--- app/components/manufacturer/manufacturer-list.component.ts
+++ app/components/manufacturer/manufacturer-list.component.ts
@@ -1,6 +1,7 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 
 import {ManufacturerRowComponent} from './manufacturer-row.component'
+import {ManufacturerService} from '../../services/manufacturer.service';
 //import { ShipService } from './ship.service';
 //import { ShipDetailComponent } from './ship-detail.component'
 
@@ -13,7 +14,21 @@ import {ManufacturerRowComponent} from './manufacturer-row.component'
     //    ShipService,
     //]
 })
-export class ManufacturerListComponent {
+export class ManufacturerListComponent implements OnInit{
     title = 'Manufacturers';
     manufacturers = [];
+    elements = 5;
+
+    constructor(private _manufacturerService:ManufacturerService) {
+    }
+
+    ngOnInit() {
+        var that = this;
+        this._manufacturerService.getManufacturers()
+            .then(function (manufacturers) {
+                for (var j = 0; (j * that.elements) < manufacturers.length; j++) {
+                    that.manufacturers[j] = manufacturers.slice(that.elements * j, that.elements * j + that.elements);
+                }
+            });
+    }
 }
