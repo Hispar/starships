08ba1b997d90e46fea331873606ddc9a13fdcd15
pburgos <pburgos@corpora360.com>
2016-04-18 17:42:13 +0200

    Home fixes

--- app/components/home.component.ts
+++ app/components/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 
 // Ships
 import {Ship} from '../models/ship'
@@ -11,39 +11,25 @@ import {ShipComponent} from './ship/ship.component'
     directives: [ShipComponent]
 })
 export class HomeComponent implements OnInit {
-    @Input()
-    ship:Ship;
-    //ship2:Ship;
-    //ship3:Ship;
-    //elements = 20;
-    //title = 'All Ships';
+    ship1:Ship;
+    ship2:Ship;
+    ship3:Ship;
+    elements = 20;
+    title = 'Star Citizen';
+    home_subtitle = 'Aquí puedes ver algunas de las naves que se encuentras disponibles en Star Citizen';
 
     constructor(private _shipService:ShipService) {
     }
 
     ngOnInit() {
-        //var that = this;
-        ////var id = Math.random() * this.elements + 1;
-        //let id = 1;
-        //this._shipService.getShip(id)
-        //    .then(ship => this.ship = ship);
-        //
-        ////this.ship1 = this._shipService.getShip(Math.floor((Math.random() * this.elements) + 1))
-        ////    .then(function (ship) {
-        ////        return ship;
-        ////    });
-        ////this._shipService.getShip(Math.floor((Math.random() * this.elements) + 1))
-        ////    .then(function (ship) {
-        ////        that.ship2 = ship;
-        ////    });
-        ////this._shipService.getShip(Math.floor((Math.random() * this.elements) + 1))
-        ////    .then(function (ship) {
-        ////        that.ship3 = ship;
-        ////    });
-        //console.log('ship:' + this.ship)
-        ////console.log('ship:' + this.ship1.manufacturer)
-        //for (var variable in this.ship) {
-        //    console.log('var:' + variable)
-        //}
+        var id = Math.floor(Math.random() * this.elements + 1);
+        this._shipService.getShip(id)
+            .then(ship => this.ship1 = ship);
+        id = Math.floor(Math.random() * this.elements + 1);
+        this._shipService.getShip(id)
+            .then(ship => this.ship2 = ship);
+        id = Math.floor(Math.random() * this.elements + 1);
+        this._shipService.getShip(id)
+            .then(ship => this.ship3 = ship);
     }
 }
