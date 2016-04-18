f3d7dd3065c6d5a5a73beba222d65fdb6da891f4
pburgos <pburgos@corpora360.com>
2016-04-18 18:01:31 +0200

    Add goToManufacturer Link

--- app/components/ship/ship-detail.component.ts
+++ app/components/ship/ship-detail.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from 'angular2/core';
-import {RouteParams} from 'angular2/router';
+import {RouteParams, Router} from 'angular2/router';
 
 import {Ship} from '../../models/ship';
 import {ShipService} from '../../services/ship.service';
@@ -12,7 +12,8 @@ export class ShipDetailComponent implements OnInit {
     ship:Ship;
 
     constructor(private _shipService:ShipService,
-                private _routeParams:RouteParams) {
+                private _routeParams:RouteParams,
+                private _router:Router) {
     }
 
     ngOnInit() {
@@ -23,5 +24,10 @@ export class ShipDetailComponent implements OnInit {
 
     goBack() {
         window.history.back();
+    }
+
+    goToManufacturer(id:number) {
+        let link = ['ManufacturerDetail', {id: id}];
+        this._router.navigate(link);
     }
 }
