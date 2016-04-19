f4dc95a683d3e8f265556ce7487999731506ae50
pburgos <pburgos@corpora360.com>
2016-04-19 11:40:00 +0200

    Add functions for home component

--- app/services/ship.service.ts
+++ app/services/ship.service.ts
@@ -6,7 +6,7 @@ import {SHIPS} from '../mocks/mock-ships';
 export class ShipService {
     getShip(id:number) {
         return Promise.resolve(SHIPS).then(
-            ships => ships.filter(ship => ship.id === id)[0]
+                ships => ships.filter(ship => ship.id === id)[0]
         );
     }
 
@@ -14,9 +14,35 @@ export class ShipService {
         return Promise.resolve(SHIPS);
     }
 
+    getRandomShip() {
+        var id = Math.floor(Math.random() * SHIPS.length + 1);
+        return Promise.resolve(SHIPS).then(
+                ships => ships.filter(ship => ship.id === id)[0]
+        );
+    }
+
+    getRandomShipCrew(crew:number, limit:number) {
+        return Promise.resolve(SHIPS).then(
+                ships => ships.filter(ship => ship.max_crew === crew).slice(0, limit)
+        );
+    }
+
     //getHeroesSlowly() {
     //    return new Promise<Ship[]>(resolve =>
     //            setTimeout(()=>resolve(SHIPS), 2000) // 2 seconds
     //    );
     //}
+
+    shuffle(array) {
+        var i = 0
+            , j = 0
+            , temp = null
+
+        for (i = array.length - 1; i > 0; i -= 1) {
+            j = Math.floor(Math.random() * (i + 1))
+            temp = array[i]
+            array[i] = array[j]
+            array[j] = temp
+        }
+    }
 }
