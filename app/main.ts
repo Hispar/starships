57281f920ac90adcfa9c367cc75c88ccd331d718
Hispar <hispar@gmail.com>
2016-04-21 00:02:01 +0200

    Add location strategy

--- app/main.ts
+++ app/main.ts
@@ -1,13 +1,4 @@
 import {bootstrap}    from 'angular2/platform/browser';
-import {provide} from 'angular2/core';
 import {AppComponent} from './app.component';
-import {
-    ROUTER_PROVIDERS,
-    LocationStrategy,
-    HashLocationStrategy
-} from 'angular2/router';
 
-bootstrap(AppComponent, [
-    ROUTER_PROVIDERS,
-    provide(LocationStrategy, {useClass: HashLocationStrategy})
-]);
+bootstrap(AppComponent);
